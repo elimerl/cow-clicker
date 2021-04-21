fcfb624885e2753ac00adf4f63ff29af24ef2f5e
elimerl <32183908+elimerl@users.noreply.github.com>
2021-04-21 17:49:57 +0000

    Deploying to gh-pages from  @ 987214c4790a2d64ea6c3043f2ade7ad8d09e17b 🚀

--- dist/Building.js
+++ dist/Building.js
@@ -61,7 +61,15 @@ export class CowFactory extends Building {
   constructor() {
     super(...arguments);
     this.cost = 1e6;
-    this.cps = 1e6;
+    this.cps = 1e5;
     this.tooltip = "A factory that builds cows. GMO-free!";
+  }
+}
+export class CowHax extends Building {
+  constructor() {
+    super(...arguments);
+    this.cost = 4e6;
+    this.cps = 4e6;
+    this.tooltip = "Hacks the game to quadruple the factories.";
   }
 }
