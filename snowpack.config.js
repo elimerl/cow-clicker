36cebb717873a8dd051b3bc412bd2f5937faabf4
elimerl <elimerlindavies@gmail.com>
2021-04-21 09:27:40 -0700

    AAA

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,17 +1,17 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
   plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-dotenv",
     [
-      '@snowpack/plugin-typescript',
+      "@snowpack/plugin-typescript",
       {
         /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? {tsc: 'yarn pnpify tsc'} : {}),
+        ...(process.versions.pnp ? { tsc: "yarn pnpify tsc" } : {}),
       },
     ],
   ],
@@ -30,6 +30,6 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    /* ... */
+    /* ... */ baseUrl: "/cow-clicker/",
   },
 };
